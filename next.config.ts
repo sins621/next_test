1088910b33fa3de75ccfeb25026baf2ad434ab1b
sins621 <sinsmailza@gmail.com>
2025-05-27 22:46:03 +0200

    fdsfdsfd

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  basePath: '/test',
+  assetPrefix: '/test/', // optional but helps with assets
+}
+
+
 export default nextConfig;

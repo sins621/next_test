a9775df488197bf9dd83a7bab1bef259947f0705
sins621 <sinsmailza@gmail.com>
2025-05-27 22:18:11 +0200

    fjsdkl

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'standalone',
+  // Your other settings go here
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
